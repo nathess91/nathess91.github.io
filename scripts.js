86b6eec491fb04972dc2f0616c826ef83812402d
Natalia Hess <nathess91@gmail.com>
2016-12-11 16:27:31 -0800

    add custom js to email button

---
+++ scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $(".social-buttons").mouseover(function() {
+    $(".social-buttons").addClass("animated infinite rubberBand");
+  });
+  $(".social-buttons").mouseout(function() {
+    $(".social-buttons").removeClass("animated infinite rubberBand");
+  });
+
+  $(".email-button").mouseover(function() {
+    // $("#envelope").addClass("fa fa-envelope-open");
+    $("#send-email").html("NataliaM.Hess@gmail.com");
+  });
+  $(".email-button").mouseout(function() {
+    // $("#envelope").removeClass("fa fa-envelope-open");
+    // $("#envelope").addClass("fa fa-envelope");
+    $("#send-email").html("Send Email");
+  });
+
+});
