74d06e171b990d4c3c0c92d3fbde3ffa713a2e06
Natalia Hess <nathess91@gmail.com>
2017-06-15 20:34:37 -0700

    add logic to make chomp swim

--- scripts.js
+++ scripts.js
@@ -1,145 +1,33 @@
-$(document).ready(function() {
-  // MAKE THEM SOCIAL ICONS BOUNCE
-  // github
-  $("#social-github").mouseover(function() {
-    $("#social-github").addClass("animated infinite rubberBand");
-  });
-  $("#social-github").mouseout(function() {
-    $("#social-github").removeClass("animated infinite rubberBand");
-  });
-  // linkedin
-  $("#social-linkedin").mouseover(function() {
-    $("#social-linkedin").addClass("animated infinite rubberBand");
-  });
-  $("#social-linkedin").mouseout(function() {
-    $("#social-linkedin").removeClass("animated infinite rubberBand");
-  });
-  // medium
-  $("#social-medium").mouseover(function() {
-    $("#social-medium").addClass("animated infinite rubberBand");
-  });
-  $("#social-medium").mouseout(function() {
-    $("#social-medium").removeClass("animated infinite rubberBand");
-  });
-  // spotify
-  $("#social-spotify").mouseover(function() {
-    $("#social-spotify").addClass("animated infinite rubberBand");
-  });
-  $("#social-spotify").mouseout(function() {
-    $("#social-spotify").removeClass("animated infinite rubberBand");
-  });
-  // twitter
-  $("#social-twitter").mouseover(function() {
-    $("#social-twitter").addClass("animated infinite rubberBand");
-  });
-  $("#social-twitter").mouseout(function() {
-    $("#social-twitter").removeClass("animated infinite rubberBand");
-  });
+function moveChomp() {
+  window.onkeydown = function(e) {
+    if (!e) {
+      e = window.event;
+    }
+    var keyPressed = e.keyCode;
+    var chomp = document.getElementById("chomp");
+    var top = parseInt(chomp.style.top, 10);
+    var left = parseInt(chomp.style.left, 10);
 
-
-  // ANIMATE EMAIL BUTTON
-  $(".email-button").mouseover(function() {
-    $("#envelope").addClass("fa fa-envelope-open");
-    $("#send-email").html("NataliaM.Hess@gmail.com");
-  });
-  $(".email-button").mouseout(function() {
-    $("#envelope").removeClass("fa fa-envelope-open");
-    $("#envelope").addClass("fa fa-envelope");
-    $("#send-email").html("Send Email");
-  });
-
-  // BUTTON PRESS COOL STUFF
-  $(".project-github").click(function() {
-    $(".project-github").css({
-      "text-shadow": "none",
-      "top": "1px",
-      "left": "1px",
-      "position": "none"
-    });
-  });
-
-  $(".email-button").click(function() {
-    $(".email-button").css({
-      "text-shadow": "none",
-      "top": "1px",
-      "left": "1px",
-      "position": "none"
-    });
-  });
-
-var isChrome = !!window.chrome && !!window.chrome.webstore;
-function Chrome() {
-  if (!isChrome) {
-    alert("This portfolio is best viewed in Chrome.");
+    if (keyPressed == 37) { // move left
+      if ( left > 0 ) {
+        chomp.style.left = left - 10 + 'px';
+      }
+    } else if (keyPressed == 38) { // move up
+      if (top > 0) {
+        chomp.style.top = top - 10 + 'px';
+      }
+    } else if (keyPressed == 39) { // move right
+      if (left + chomp.width + 10 < window.innerWidth) {
+        chomp.style.left = left + 10 + 'px';
+      }
+    } else if (keyPressed == 40) { // move down
+      if (top + chomp.height + 10 < window.innerHeight) {
+        chomp.style.top = top + 10 + 'px';
+      }
+    }
   }
 }
 
-$('.horizontal-scroll').slick({
-dots: true,
-infinite: true,
-speed: 300,
-slidesToShow: 4,
-slidesToScroll: 3,
-autoplay: true,
-autoplaySpeed: 3000,
-responsive: [
-  {
-    breakpoint: 1024,
-    settings: {
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      infinite: true,
-      dots: true
-    }
-  },
-  {
-    breakpoint: 600,
-    settings: {
-      slidesToShow: 3,
-      slidesToScroll: 3
-    }
-  },
-  {
-    breakpoint: 480,
-    settings: {
-      slidesToShow: 2,
-      slidesToScroll: 2
-    }
-  }
-  // You can unslick at a given breakpoint now by adding:
-  // settings: "unslick"
-  // instead of a settings object
-]
-});
-
-});
-
-// NAV SCROLL TO SECTION
-
 $(document).ready(function() {
-
-  $("#about").click(function() {
-      $('html,body').animate({
-          scrollTop: $("#about-section").offset().top},
-          'slow');
-  });
-
-  $("#projects").click(function() {
-      $('html,body').animate({
-          scrollTop: $("#projects-section").offset().top},
-          'slow');
-  });
-
-  $("#experience").click(function() {
-      $('html,body').animate({
-          scrollTop: $("#experience-section").offset().top},
-          'slow');
-  });
-
-  $("#contact").click(function() {
-      $('html,body').animate({
-          scrollTop: $("#contact-section").offset().top
-        },'slow');
-  });
-
+  moveChomp();
 });
