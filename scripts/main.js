aee3889a56e3a69afd98c65df1fef7a662e4deef
Natalia Hess <nathess91@gmail.com>
2017-06-16 23:43:09 -0700

    animate chomp on load

---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+function animateChomp() {
+   $("#chomp").animate({
+     left: $(window).innerWidth() - 100
+   }, 12000, function() {
+     $("#chomp").addClass("flip-chomp");
+     $("#chomp").animate({
+       left: 0
+     }, 12000);
+   });
+   $("#chomp").removeClass("flip-chomp");
+}
+
+function moveChomp() {
+  window.onkeydown = function(e) {
+    $("#chomp").stop();
+    if (!e) {
+      e = window.event;
+    }
+    var keyPressed = e.keyCode;
+    var top = parseInt(chomp.style.top, 10);
+    var left = parseInt(chomp.style.left, 10);
+
+    if (keyPressed == 37) { // move left
+      if (left > 0) {
+        $("#chomp").addClass("flip-chomp");
+        chomp.style.left = left - 20 + 'px';
+      }
+    } else if (keyPressed == 38) { // move up
+      if (top > 0) {
+        chomp.style.top = top - 20 + 'px';
+      }
+    } else if (keyPressed == 39) { // move right
+      if (left + chomp.width + 10 < window.innerWidth) {
+        $("#chomp").removeClass("flip-chomp");
+        chomp.style.left = left + 20 + 'px';
+      }
+    } else if (keyPressed == 40) { // move down
+      if (top + chomp.height + 10 < window.innerHeight) {
+        chomp.style.top = top + 20 + 'px';
+      }
+    }
+  }
+}
+
+$(document).ready(function() {
+  animateChomp();
+  setInterval(function() {
+    animateChomp();
+  }, 24000);
+  moveChomp();
+});
